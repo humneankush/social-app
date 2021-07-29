1e40a631e079353f48989b89f6671e48573b913a
humneankush <humneankush@gmail.com>
2021-07-29 15:46:19 +0530

    frontend is completed

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import "./home.css";
+import Rightbar from "../../components/rightbar/Rightbar";
+
+export default function Home() {
+  return (
+    <>
+      <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
+  );
+}
