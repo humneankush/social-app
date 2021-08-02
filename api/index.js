5a986718f4609de6b828c9a8ccd89f9f3e73699e
humneankush <humneankush@gmail.com>
2021-08-02 15:08:27 +0530

    posts model and its router is completed

---
+++ api/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const dotev = require("dotenv").config();
+
+const mongoose = require("mongoose");
+const app = express();
+
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log({ msg: "connected to mongo cloud" });
+  })
+  .catch((err) => {
+    msg: err.message;
+  });
+
+// middle ware
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/post", postRoute);
+
+app.listen(process.env.PORT, () => {
+  console.log("server is listening now");
+});
